4fb91f8bc301f82d77c51e3ea3514f1eee34e203
Connor Macdougall <cmacdougall78@gmail.com>
2023-03-09 12:47:40 +0200

    Added status diagnostics endpoint

--- src/service_stellar_signer/urls.py
+++ src/service_stellar_signer/urls.py
@@ -7,7 +7,10 @@ from . import views
 urlpatterns = (
     # Admin
     re_path(r'^admin/transactions/$', views.AdminTransactionView.as_view(), name='admin-transactions'),
-    re_path(r'^admin/wallet/$', views.AdminWalletView.as_view(), name='admin-wallet')
+    re_path(r'^admin/wallet/$', views.AdminWalletView.as_view(), name='admin-wallet'),
+    
+    # Public
+    re_path(r'^status/$', views.StatusView.as_view(), name='status')
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
